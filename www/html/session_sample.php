188c6b82c89550246a88f76d7642e2f4a9780117
Moemi02 <mo.araki0227@gmail.com>
2020-03-01 11:44:07 +0900

    cookie_sample.phpとsession_sample.phpの追加

---
+++ www/html/session_sample.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>Session</title>
+  </head>
+  <body>
+    <?php
+    
+    print 'セッション名: ' . session_name() . "<br>";
+    print 'セッションID: ' . session_id() .  "<br>";
+    if (isset($_SESSION['count'])) {
+       $_SESSION['count']++;
+    } else {
+       $_SESSION['count'] = 1;
+    }
+    print $_SESSION['count'] . '回目の訪問です' .  "<br>";
+    ?>
+  </body>
+</html>
